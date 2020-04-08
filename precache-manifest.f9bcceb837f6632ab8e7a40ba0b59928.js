e187249bbd78255cc73e1991392ee4a4b7acb297
krimalex <63273819+krimalex@users.noreply.github.com>
2020-04-08 16:13:20 +0300

    Add files via upload

---
+++ precache-manifest.f9bcceb837f6632ab8e7a40ba0b59928.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "17955480b3522c83fbdd",
+    "url": "/static/css/main.0efd88c5.chunk.css"
+  },
+  {
+    "revision": "17955480b3522c83fbdd",
+    "url": "/static/js/main.e736f55a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a52456f7306b284c46b0",
+    "url": "/static/css/2.29bfc4e5.chunk.css"
+  },
+  {
+    "revision": "a52456f7306b284c46b0",
+    "url": "/static/js/2.c5605f72.chunk.js"
+  },
+  {
+    "revision": "eac85ed2ee07fec21542e2612be5ee45",
+    "url": "/index.html"
+  }
+];
